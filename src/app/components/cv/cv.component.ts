9e0883ce2207add6015a6135ba1d025623cacfcf
Maizi <xamdiego2@hotmail.com>
2024-09-29 22:25:49 +0200

    trying to fix css for mobile

--- src/app/components/cv/cv.component.ts
+++ src/app/components/cv/cv.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
-import {NgOptimizedImage, NgTemplateOutlet} from "@angular/common";
+import {Component, HostListener, OnInit} from '@angular/core';
+import {NgIf, NgOptimizedImage, NgTemplateOutlet} from "@angular/common";
 
 @Component({
   selector: 'app-cv',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    NgTemplateOutlet
+    NgTemplateOutlet,
+    NgIf
   ],
   templateUrl: './cv.component.html',
   styleUrl: './cv.component.scss'
 })
-export class CvComponent {
+export class CvComponent implements OnInit{
+
+  isSmallScreen: boolean = false;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.isSmallScreen = window.innerWidth < 600;
+  }
+
+  ngOnInit() {
+    this.isSmallScreen = window.innerWidth < 600; // Initial check
+  }
 
 }
