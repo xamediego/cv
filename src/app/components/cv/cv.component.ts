9c14ebb598460d5368834de567566018b9fa04a0
Altair <max.de.jonge@live.nl>
2025-06-18 13:18:39 +0200

    Styling update

--- src/app/components/cv/cv.component.ts
+++ src/app/components/cv/cv.component.ts
@@ -1,28 +1,28 @@
 import {Component, HostListener, OnInit} from '@angular/core';
-import {NgIf, NgOptimizedImage, NgTemplateOutlet} from "@angular/common";
+import {NgIf, NgTemplateOutlet} from "@angular/common";
 
 @Component({
   selector: 'app-cv',
   standalone: true,
   imports: [
-    NgOptimizedImage,
     NgTemplateOutlet,
     NgIf
   ],
   templateUrl: './cv.component.html',
-  styleUrl: './cv.component.scss'
+  styleUrl: './cv.component.scss',
 })
 export class CvComponent implements OnInit{
+
 
   isSmallScreen: boolean = false;
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
-    this.isSmallScreen = window.innerWidth < 600;
+    this.isSmallScreen = window.innerWidth < 768;
   }
 
   ngOnInit() {
-    this.isSmallScreen = window.innerWidth < 600; // Initial check
+    this.isSmallScreen = window.innerWidth < 768; // Initial check
   }
 
 }
