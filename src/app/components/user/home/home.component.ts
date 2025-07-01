0d03ebb8a7ce90a86ef65a11e19549c5fb8cb6c5
Altair <max.de.jonge@live.nl>
2025-07-01 12:43:20 +0200

    Home wip

--- src/app/components/user/home/home.component.ts
+++ src/app/components/user/home/home.component.ts
@@ -9,19 +9,19 @@ import {RouterLink} from "@angular/router";
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
 
-  featuredProjects = [
-    { title: 'VCTF-Retribution', image: 'assets/projects/R_1.jpg' },
-    { title: 'VCTF-Retribution', image: 'assets/projects/R_2.jpg' },
-    { title: 'VCTF-Retribution', image: 'assets/projects/R_3.jpg' },
-    { title: 'Healthmate', image: 'assets/projects/H_1.jpg' },
-    { title: 'Healthmate', image: 'assets/projects/H_2.jpg' },
+  public featuredProjects = [
+    {title: 'VCTF-Retribution', image: 'assets/projects/R_1.jpg'},
+    {title: 'VCTF-Retribution', image: 'assets/projects/R_2.jpg'},
+    {title: 'VCTF-Retribution', image: 'assets/projects/R_3.jpg'},
+    {title: 'Healthmate', image: 'assets/projects/H_1.jpg'},
+    {title: 'Healthmate', image: 'assets/projects/H_2.jpg'},
   ];
 
-  activeIndex = 0;
+  public activeIndex = 0;
 
-  ngOnInit() {
+  public ngOnInit() {
     setInterval(() => {
       this.activeIndex = (this.activeIndex + 1) % this.featuredProjects.length;
     }, 4000);
