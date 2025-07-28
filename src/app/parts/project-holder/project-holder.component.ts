dc28d20584c98fb5b42c722e5862941cf06ad568
Itazuki <max.de.jonge@live.nl>
2025-07-28 14:42:41 +0200

    project display ref for reuse

---
+++ src/app/parts/project-holder/project-holder.component.ts
@@ -0,0 +1,25 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+import {Project} from "../../services/entities/project";
+import {ProjectType} from "../../services/entities/project.type";
+
+@Component({
+    selector: 'project-holder',
+    templateUrl: './project-holder.component.html',
+    styleUrls: ['./project-holder.component.scss'],
+    imports: []
+})
+export class ProjectHolderComponent {
+
+  @Input() project: Project | null = null;
+  @Input() projectType: ProjectType | null = null;
+  @Input() onProjectSelect : (projectType: ProjectType, project: Project) => void = () => {};
+  @Output() loaded : EventEmitter<boolean> = new EventEmitter();
+
+  onImageLoad() {this.loaded.emit(true);}
+
+  createProjectString(publishedDate: Date) {
+    const date = new Date(publishedDate);
+    return `Created:${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+  }
+}
